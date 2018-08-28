da764a81271c76eb7d57b696e4352b9bf2d61c4c
Acrelion <iachi1763@gmail.com>
2018-08-29 00:04:02 +0300

    Include new datetime picker lib, BS4, scripts, js custom file

--- project/portal/forms.py
+++ project/portal/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from datetimewidget.widgets import DateTimeWidget
 
 from .models import MissModel
 
@@ -20,14 +19,3 @@ class SubmitForm(forms.ModelForm):
     class Meta:
         model = MissModel
         fields = ("from_date", "to_date", "type_miss")
-        dateTimeOptions = {
-            'format': 'dd/mm/yyyy HH:ii P',
-            'autoclose': True,
-            'showMeridian' : True,
-            'attrs': {'id':"yourdatetimeid"}
-        }
-
-        widgets = {
-            #Use localization and bootstrap 3
-            'datetime': DateTimeWidget(options=dateTimeOptions)
-        }
---
+++ project/static/portal/js/main.js
@@ -0,0 +1,4 @@
+$(function () {
+    $('#id_from_date').datetimepicker();
+    $('#id_to_date').datetimepicker();
+});
